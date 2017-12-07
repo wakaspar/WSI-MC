269c1192136b1e0c8b7d56e5755283d0aa5a651c
Will Kaspar <Will@Aang.local>
2017-12-06 21:52:33 -0800

    reorganizes files structure, adds normalize.css for sure this time.  Nevermind that last commit, that commit's a lair ;)

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+// This is where the JavaScript for the Williams-Sonoma Markup Challege for
+// William Kaspar will live.  It will run some of the actions on the page,
+// switching images and expandables-collapsables.
+
+console.log('app.js working. hello world!');
