f0579a57a0ebddbcb525f343a53a07baa4958e5e
Will Kaspar <Will@Aang.local>
2017-12-09 22:06:52 -0800

    responsive css in place, jquery added and initialized, removed normalize.css - just didn't need it anymore

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,24 @@
 // William Kaspar will live.  It will run some of the actions on the page,
 // switching images and expandables-collapsables.
 
-console.log('app.js working. hello world!');
+
+$(document).ready(function(){
+  console.log('app.js working. hello world!');
+
+  $('.sidekick').on('click', function(){
+    console.log('hero switch!');
+  });
+
+  $('#add-to-cart').on('click', function(){
+    console.log('quantity added!');
+  });
+
+  $('.expanded').on('click', function(){
+    console.log('collapsed!');
+  });
+
+  $('.collapsed').on('click', function(){
+    console.log('expanded!');
+  });
+
+});
