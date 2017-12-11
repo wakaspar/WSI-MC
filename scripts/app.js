9be12b7fbf9a5c3204095b150dee37061814f652
Will Kaspar <Will@Aang.local>
2017-12-11 01:01:51 -0800

    all kinds of stuff, a mad dash to the finish

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,60 @@
 // This is where the JavaScript for the Williams-Sonoma Markup Challege for
-// William Kaspar will live.  It will run some of the actions on the page,
-// switching images and expandables-collapsables.
-
+// Will Kaspar will live.  This version utilizes jQuery.  It will run some of
+// the actions on the page, switching images, and expandables-collapsables.
 
 $(document).ready(function(){
-  console.log('app.js working. hello world!');
 
-  $('.sidekick').on('click', function(){
-    console.log('hero switch!');
-  });
+  $('.add-to-cart-modal').hide();
 
+  /* NOTE: Grabs quantity from input field and builds #items-added message */
+  function displayItemsAdded(){
+    var howMany = $('#quantity').val();
+    if (howMany == 0) {
+      $('.items-added').text(`Error: Please select a quantity.`);
+      $('.modal-greeting').text(`Uh-oh.  Try again!`);
+    } else if (howMany > 1) {
+      $('.items-added').text(`You have added ${howMany} items to your cart.`);
+    } else {
+      $('.items-added').text(`You have added ${howMany} item to your cart.`);
+    }
+    $('.items-added').css('display', 'inline');
+  };
+
+  /* NOTE: Displays quantity being added on #add-to-cart button press,
+  in a message and on a modal */
   $('#add-to-cart').on('click', function(){
-    console.log('quantity added!');
+    displayItemsAdded();
+    $('.add-to-cart-modal').show();
+    $('.modal-overlay').css('display', 'block');
   });
 
-  $('.expanded').on('click', function(){
-    console.log('collapsed!');
+  $('#button-close-modal').on('click', function(){
+    $('.add-to-cart-modal').hide();
+    $('.modal-overlay').css('display', 'none');
+  })
+
+  /* NOTE: Switches #hero image with a selected .sidekick */
+  $('.sidekick').on('click', function(){
+    var oldSidekick = $('.sidekick.active').removeClass('active');
+    var newHero = $(this).attr('hero');
+    $(this).addClass('active');
+    $('#hero').attr('src', newHero);
   });
 
-  $('.collapsed').on('click', function(){
-    console.log('expanded!');
-  });
+  /* NOTE: Toggles expandable-collapsable sections */
+  $('#accordion .acc-box').on('click', function(){
+    var accHead = $(this).find('h3');
+    var accBody = $(this).find('p');
+    var expArrow = 'assets/arrow-expanded.png';
+    var colArrow = 'assets/arrow-collapsed.png';
 
+    if (accHead.text() == 'EXPANDED') {
+      accHead.text('COLLAPSED')
+      $(this).find('img').attr('src', colArrow);
+    } else {
+      accHead.text('EXPANDED');
+      $(this).find('img').attr('src', expArrow);
+    }
+    accBody.toggle('slide');
+  });
 });
